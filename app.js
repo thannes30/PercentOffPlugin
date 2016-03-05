0aa0a5a7526e701fed8ed317eaab20ca34ef6af0
thannes30 <thannes30@yahoo.com>
2016-03-05 13:51:21 -0500

    takin a break

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ $("ProductList > li").each(function(){
   var prices = $(this).find("p.price").text();
   var retail = prices.split(" ")[0];
   var sale = prices.split(" ")[1];
-  var
+  var percent = (sale - retail) * 100 / sale;
 })
