08566a6dd12a3eb46b3a12677beab263fadae884
thannes30 <thannes30@yahoo.com>
2016-03-06 10:39:12 -0500

    appending span with percent off to each li

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-$("ProductList > li").each(function(){
-  var prices = $(this).find("p.price").text();
-  var retail = prices.split(" ")[0];
-  var sale = prices.split(" ")[1];
-  var percent = (sale - retail) * 100 / sale;
-})
+function percentOff(){
+    $(".ProductList li").each(function(){
+      var prices = $(this).find("em.p-price").text().trim();
+      var retail = prices.split(" ")[0].replace("$","");
+      var sale = prices.split(" ")[1].replace("$","");
+        var percentRaw = (retail - sale) / retail * 100;
+        var percentOff = Math.round(percentRaw);
+
+        if( percentOff <= 10 ){
+          $(this).find('.RetailPriceValue').addClass("hide");
+        }
+    })
+}
+percentOff();
