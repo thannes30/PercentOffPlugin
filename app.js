f464a7c99b580b995ca83b8ff6a0294de8cd4b30
thannes30 <thannes30@yahoo.com>
2016-03-06 10:54:27 -0500

    appending span to li site-wide

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+// APPEND PERCENT OFF SPAN IF PERCENT OFF IS > 10%
 function percentOff(){
     $(".ProductList li").each(function(){
-      var prices = $(this).find("em.p-price").text().trim();
+      var prices = $(this).find("em.p-price").text();
       var retail = prices.split(" ")[0].replace("$","");
       var sale = prices.split(" ")[1].replace("$","");
         var percentRaw = (retail - sale) / retail * 100;
         var percentOff = Math.round(percentRaw);
 
-        if( percentOff <= 10 ){
-          $(this).find('.RetailPriceValue').addClass("hide");
+        if( percentOff >= 10 ){
+          $(this).append('<span class="calc-percent-off">' + percentOff + '% OFF</span>')
         }
     })
 }
